ca930865412493d6b98b4541c294f0eeddacc952
Nhan Nguyen <nathan13574@gmail.com>
2020-08-28 03:08:12 -0500

    commit

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BoxingUnboxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var objList = new List<object>
+            {
+                7,
+                28,
+                -1,
+                true,
+                "chair"
+            };
+            foreach (var obj in objList)
+            {
+                Console.WriteLine(obj);
+            }
+
+            int sum = 0;
+            foreach (var obj in objList)
+                if (obj is int)
+                {
+                    {
+                        sum += (int)obj;
+                    }
+                }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
